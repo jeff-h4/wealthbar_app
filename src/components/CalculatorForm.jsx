0d36760ce1f36f80e68d5a857b23d4d35b869814
Jeff H <jeff.h.four@gmail.com>
2019-02-15 18:04:33 -0800

    Validated form input

--- src/components/CalculatorForm.jsx
+++ src/components/CalculatorForm.jsx
@@ -17,15 +17,12 @@ export default class CalculatorForm extends React.Component {
 
     this.onChange     = this.onChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.printValues  = this.printValues.bind(this);
   }
 
   onChange(event) {
-    this.setState({[event.target.name]: event.target.value});
-  }
-
-  printValues() {
-    console.log(this.state);
+    let floatVal = parseFloat(event.target.value);
+    if (isNaN(floatVal) || floatVal < 0) return;
+    this.setState({[event.target.name]: floatVal});
   }
 
   handleSubmit(event) {
@@ -50,32 +47,62 @@ export default class CalculatorForm extends React.Component {
       <Form onSubmit={this.handleSubmit}>
         <Form.Group>
           <Form.Label>Current Marginal Tax Rate (%):</Form.Label>
-          <Form.Control size="sm" type="text" name="currentMarginalTaxRate" value={this.state.currentMarginalTaxRate} onChange={this.onChange} />
+          <Form.Control
+            size="sm"
+            type="text"
+            name="currentMarginalTaxRate"
+            value={this.state.currentMarginalTaxRate}
+            onChange={this.onChange} />
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Future Marginal Tax Rate (%):</Form.Label>
-          <Form.Control size="sm" type="text" name="futureMarginalTaxRate" value={this.state.futureMarginalTaxRate} onChange={this.onChange} />
+          <Form.Control
+             size="sm"
+             type="text"
+             name="futureMarginalTaxRate"
+             value={this.state.futureMarginalTaxRate}
+             onChange={this.onChange} />
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Principal ($):</Form.Label>
-          <Form.Control size="sm" type="text" name="principal" value={this.state.principal} onChange={this.onChange} />
+          <Form.Control
+            size="sm"
+            type="text"
+            name="principal"
+            value={this.state.principal}
+            onChange={this.onChange} />
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Duration (years):</Form.Label>
-          <Form.Control size="sm" type="text" name="duration" value={this.state.duration} onChange={this.onChange} />
+          <Form.Control
+            size="sm"
+            type="text"
+            name="duration"
+            value={this.state.duration}
+            onChange={this.onChange} />
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Return on Investment Rate (%):</Form.Label>
-          <Form.Control size="sm" type="text" name="returnRate" value={this.state.returnRate} onChange={this.onChange} />
+          <Form.Control
+            size="sm"
+            type="text"
+            name="returnRate"
+            value={this.state.returnRate}
+            onChange={this.onChange} />
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Inflation Rate (%):</Form.Label>
-          <Form.Control size="sm" type="text" className="form-control" name="inflationRate" value={this.state.inflationRate} onChange={this.onChange} />
+          <Form.Control
+            size="sm"
+            type="text"
+            name="inflationRate"
+            value={this.state.inflationRate}
+            onChange={this.onChange} />
         </Form.Group>
 
         <Button variant="primary" type="submit">Submit</Button>
